5c8cbab59cde4723075fb102bc3e6a7290a2ff1c
DeathCode <dc.deathcode@gmail.com>
2025-04-09 18:20:16 +0530

    2 Syntax and First Program

---
+++ 2_Syntax_FirstProgram.py
@@ -0,0 +1,17 @@
+
+# print("Hello, World by DeathCode")
+
+# if 5 > 2:
+#     print("Five is greater than two!")
+#     print("another line")
+"""
+lakjsdf;lkajsd
+Multi line
+comment
+"""
+
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+sum = num1 + num2
+print("Sum is:",sum,num1,num2)
