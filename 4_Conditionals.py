5b2adbd4e95fbd9bed4e9dffcee87211c393f044
DeathCode <dc.deathcode@gmail.com>
2025-04-10 15:07:26 +0530

    Conditional Statements with detials

---
+++ 4_Conditionals.py
@@ -0,0 +1,32 @@
+# if statement
+
+# age = 17
+
+# if age >= 18:
+#     print("You can Drive Everything!")
+#     if True:
+#         print("YES this is true.")
+#     else:
+#         print("No this is not true")
+# elif age>=15:
+#     print("You can Only Drive 2 wheelers!")
+# else:
+#     print("Your age is not valid for self driving")
+
+username = "deathcode"
+password = "123456"
+
+# if password == "1234":
+#     pass
+
+if username == "deathcode":
+    if password == "12345":
+        print("Login Successful")
+    else:
+        print("Wrong password")
+else:
+    print("Invalid Username.")
+
+
+
+# print("Program after if")
