6e94f230f22248b08a31c09d49d2c2f44fc0fb5b
DeathCode <dc.deathcode@gmail.com>
2025-04-10 14:05:11 +0530

    Variables and Data Types Detial

---
+++ 3_Var_DataTypes.py
@@ -0,0 +1,79 @@
+# x = 10
+# y = "string"
+# z = [1,2]
+# g = None
+
+# print(type(5))
+# print(type("5")) 
+# print(type(3.14)) 
+# print(type(True)) 
+# print(type([1,2])) 
+# print(type((1,2))) 
+# print(type({"channel":"DeathCode"})) 
+# print(type({1,2,3,4,4})) 
+# print(type(None)) 
+
+
+
+# a = 5
+# b = 3.14
+# c = 3 + 4j
+
+# print(a+b)
+# print(c.imag)
+# print(c.real)
+
+# String
+
+# name = "DeathCode"
+# print(name)
+# print(name.upper())
+# print(name.lower())
+# print(name[8])
+# print(name[0:4])
+# print("Code" in name)
+
+# Bool
+# a = True
+
+# List
+# nums = [1,2,3,4,5,None,"Hello",True,["death","code"],2.23]
+# print(nums)
+# nums.append(101)
+# nums.remove("Hello")
+# print(len(nums[6]))
+
+# Tuples
+# tp = (1,2,3,4)
+# tp2 = (1,)
+# print(tp[3])
+# print(type(tp2))
+
+
+# Set
+# s = {1,2,3,4,4,5,3,7}
+# print(s)
+
+# Dict
+# user = {
+#     "name":"DeathCode",
+#     "age":45,
+#     "channel":{
+#         "name":"DeathCode Channel",
+#         "Subs": 27000
+#     }
+# }
+# user["location"] = "Delhi"
+# user['name'] = "Death"
+# del user['age']
+# print(user)
+
+# r = range(2,6)
+# print(list(r))
+
+# print(dir(str))
+l = [1,2,3,4,5]
+print(sum(l))
+t = (2,3)
+a,b = t
+print(a,b)
