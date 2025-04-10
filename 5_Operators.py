a46648e0cd58719ac64bd9f51fd77479386a983b
DeathCode <dc.deathcode@gmail.com>
2025-04-10 15:30:16 +0530

    Operators Details and Falsy Values

---
+++ 5_Operators.py
@@ -0,0 +1,40 @@
+# a = 5
+# a = a+ 1
+# a
+# 6
+# a += 1
+# a
+# 7
+# a = [1,2,3]
+# b = a
+# a
+# [1, 2, 3]
+# b
+# [1, 2, 3]
+# c = [1,2,3]
+# a==c
+# True
+# a is c
+# False
+# a is b
+# True
+# fruits = ['apple','banan','mango']
+# fruits
+# ['apple', 'banan', 'mango']
+
+# if 'grapes' in fruits:
+#     print("yes")
+
+    
+# if 'banan' in fruits:
+#     print("
+          
+# SyntaxError: unterminated string literal (detected at line 2)
+# if 'banan' in fruits:
+#     print("Yes")
+
+          
+# Yes
+
+# Falsy values
+# 0, "", [], {}, None => False
