34244abd72f343c6dab1fca41bce9b70c5978b59
DeathCode <dc.deathcode@gmail.com>
2025-04-13 17:37:54 +0530

    Scope and execution context

---
+++ 8_Scope_EC.py
@@ -0,0 +1,19 @@
+
+# fun() # this will cause NameError
+
+# def fun():
+#     myName = "Death"
+#     age = 77
+#     print(myName ,age)
+
+# add = lambda a,b : a+b
+
+# print(add(5,8))
+
+def greet():
+    print("Hello")
+
+greet_lambda = lambda:print("Hi")
+
+greet()
+greet_lambda()
