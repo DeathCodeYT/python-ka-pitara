bf0eac8f0f77020e4a3a1efd82f5b30ef702fdac
DeathCode <dc.deathcode@gmail.com>
2025-04-14 14:28:39 +0530

    Modules and Virutal ENV

---
+++ 14_Modules_pip.py
@@ -0,0 +1,16 @@
+# Built-in vs CustomModules
+# (math,random,os,time,...etc) => Built-in
+
+import myModules
+
+# myModules.greet("DeathCode")
+# print(f"this is From Project File: {__name__}")
+
+print(myModules.age)
+# import requests
+import pandas
+import sys
+print("hello")
+print(sys.executable)
+
+# pip => Python Package Installer/Manager
