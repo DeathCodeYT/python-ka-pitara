9ec9bbf259278099ad9d0c9a0798c247c88f60b0
DeathCode <dc.deathcode@gmail.com>
2025-04-22 19:58:31 +0530

    Execution model in pyhton.. How python read & execute code

---
+++ 9_Execution_Model.py
@@ -0,0 +1,20 @@
+x = 5
+def greet(name):
+    print("Hello")
+
+# greet("DeathCode")
+# print(x)
+
+def fun():
+    if 54 >= 9:
+        def add(a,b):
+            p = 47
+            z = 99
+            return a+b
+    s = add(66,66)
+    print(s)
+
+fun()
+
+# s = add(33,33)
+# print(s)
