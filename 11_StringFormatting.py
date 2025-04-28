3c664b5bc6f1078448b49f86605411af2d97e860
DeathCode <dc.deathcode@gmail.com>
2025-04-28 12:44:07 +0530

    String Formatting with advnace tips

---
+++ 11_StringFormatting.py
@@ -0,0 +1,47 @@
+
+
+name = "Ravi"
+age = 25
+
+# print("Hello, %s! you are %d years old." % (name,age))
+
+# print("Hello, {}! you are {} years old".format(name,age))
+# print("Hello, {1}! you are {0} years old".format(name,age))
+# print("Hello, {name}! you are {age} years old".format(name=name,age=age))
+
+# print("{:>10}".format(name))
+# print("{:<10}".format(name))
+
+# print("Value of PI is {:>10.2f}".format(3.14159))
+
+person = {
+    'name':"Ravi",
+    'age':25
+}
+
+# print(f"Hello, {name}! you are {age} years old.")
+# print(f"2 + 2 = {2+2}")
+
+# print(f"Hello, {person["name"]}! you are {person['age']} years old.")
+
+
+# print(f"{name=}")
+# price = 123.456
+# print(f"price: {price:.2f}")
+# print(f"hex: {255255255:x}")
+# print(f"Binary: {255:b}")
+
+# print(f"{"Hello":>10}")
+# print(f"{"Hello":^10}")
+# print(f"{"Hello":<10}")
+
+
+# print(f"{100000000000:,}")
+
+# print(f"""
+# Hello I m {"DeathCode"}
+# """)
+
+print("Hello,d\"n\" I m DeathCode")
+
+
