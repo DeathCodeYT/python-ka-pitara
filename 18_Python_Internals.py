fc8f85a9de8bbb2c172b46d719c2e73e32406832
DeathCode <dc.deathcode@gmail.com>
2025-05-15 13:13:56 +0530

    Object Model in python

---
+++ 18_Python_Internals.py
@@ -0,0 +1,28 @@
+
+
+
+x = 3456745678
+# y = x
+# x += 1
+
+
+# print(id(x))
+# print(type(x))
+# print(dir(x))
+# print(id(y))
+
+# print(isinstance(10,int))
+# print(isinstance("sdf",int))
+# print(isinstance([1,2],object))
+# print(type(object))
+
+
+# print("5"+4)
+# small integers (5,256)
+# short string: 'yes','not',
+
+def myfun():
+    return 55
+
+# print(dir(myfun))
+print(myfun.__name__)
