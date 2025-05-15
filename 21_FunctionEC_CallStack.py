cf5e11b1eec0b7cf694440d828120905e1f2a3a9
DeathCode <dc.deathcode@gmail.com>
2025-05-15 16:46:21 +0530

    Function Execution & Call stack deep dive

---
+++ 21_FunctionEC_CallStack.py
@@ -0,0 +1,31 @@
+
+# def add(a,b):
+#     result = a+b
+#     return result
+
+# def main():
+#     total = add(5,7)
+#     print(total)
+
+# main()
+
+
+
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+
+# print(factorial(4))
+
+
+# def infinite_recursion():
+    
+#     infinite_recursion()
+
+# infinite_recursion()
+
+import sys
+
+print(sys.getrecursionlimit())
